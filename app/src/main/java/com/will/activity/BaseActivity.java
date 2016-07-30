d2295e8cb731d314bca223885423d4436ff782b5
will <467184813@qq.com>
2016-07-30 17:34:03 +0800

    Initial commit

---
+++ app/src/main/java/com/will/activity/BaseActivity.java
@@ -0,0 +1,35 @@
+package com.will.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by will on 2016/7/29.
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/will/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.will.activity;
+
+
+import android.os.Bundle;
+
+import com.will.zero.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
